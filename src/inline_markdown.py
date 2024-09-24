aba5f5bbc055bac1c5e91d379d3f9c9791c5fce4
Jakub F. Bortlík <jakub.bortlik@proton.me>
2024-09-24 09:14:25 +0200

    refactor: rename return variable

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -7,10 +7,10 @@ def split_nodes_delimiter(
     text_type: TextType
 ) -> list[TextNode]:
     """Split TextType.TEXT nodes according to the delimiter."""
-    new_nodes: list[TextNode] = []
+    result: list[TextNode] = []
     for node in old_nodes:
         if node.text_type != TextType.TEXT:
-            new_nodes.append(node)
+            result.append(node)
             continue
         split_text = node.text.split(delimiter)
         if len(split_text) % 2 == 0:
@@ -18,10 +18,10 @@ def split_nodes_delimiter(
         split_text_iter = iter(split_text)
         for part in split_text_iter:
             if part:
-                new_nodes.append(TextNode(part, TextType.TEXT))
+                result.append(TextNode(part, TextType.TEXT))
             try:
                 special_node = next(split_text_iter)
             except StopIteration:
                 continue
-            new_nodes.append(TextNode(special_node, text_type))
-    return new_nodes
+            result.append(TextNode(special_node, text_type))
+    return result
