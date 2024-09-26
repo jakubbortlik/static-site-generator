9cb5256493b54d2a3ef826c73407cdb163a8e001
Jakub F. Bortlík <jakub.bortlik@proton.me>
2024-09-26 10:30:44 +0200

    fix: define __eq__ for HTMLNode parent class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,6 +33,14 @@ class HTMLNode:
     def __repr__(self):
         return f'{self.__class__.__name__}(tag="{self.tag}", value="{self.value}", children={self.children}, props={self.props})'
 
+    def __eq__(self, other):
+        return (
+            self.tag == other.tag
+            and self.value == other.value
+            and self.children == other.children
+            and self.props == other.props
+        )
+
 
 class LeafNode(HTMLNode):
     def __init__(
@@ -46,14 +54,6 @@ class LeafNode(HTMLNode):
         if self.tag is None or self.tag == "":
             return str(self.value)
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
-
-    def __eq__(self, other):
-        return (
-            self.tag == other.tag
-            and self.value == other.value
-            and self.children == other.children
-            and self.props == other.props
-        )
 
 
 class ParentNode(HTMLNode):
