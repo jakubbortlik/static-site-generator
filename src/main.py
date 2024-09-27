ac1fed0adb7cf8eee52d087c7e9f20e30ddcb6a5
Jakub F. Bortlík <jakub.bortlik@proton.me>
2024-09-27 17:40:21 +0200

    feat: add function for deleting public directory

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
-from textnode import TextNode
+import os
+import shutil
+
+
+def copy_directory_content(source, destination, level):
+    if os.path.exists(destination):
+        print(f"{' ' * level}deleting existing destination folder `{destination}`")
+        shutil.rmtree(destination)
+
+    print(f"{' ' * level}creating destination folder `{destination}`")
+    os.mkdir(destination)
+
+    print(f"{' ' * level}copying from `{source}` to `{destination}`:")
+    level += 1
+    for file in os.listdir(source):
+        full_path = os.path.join(source, file)
+        dest_path = os.path.join(destination, file)
+        if os.path.isfile(full_path):
+            print(f"{' ' * level}cp {full_path} -> {dest_path}")
+            shutil.copy(full_path, dest_path)
+        else:
+            copy_directory_content(full_path, dest_path, level)
 
 
 def main():
-    tn = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(tn)
+    copy_directory_content("static", "public", 0)
 
 
 if __name__ == "__main__":
